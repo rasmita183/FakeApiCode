42e3c7c96edf3f27832a5be8a2f7b873b66f23d3
Rasmita Choudhury <55849665+rasmita183@users.noreply.github.com>
2025-02-08 21:16:07 +0000

    Update getAllProducts.js

--- test/products/getAllProducts.js
+++ test/products/getAllProducts.js
@@ -6,14 +6,4 @@ zest.describe("Get All Products", function () {
         zest.expect(response.status).toBe(200);
         zest.expect(response.body).toBeArray();
     });
-
-    zest.it("Performance Test: Fetch all products multiple times", async function () {
-        let startTime = Date.now();
-        for (let i = 0; i < 10; i++) {
-            const response = await zest.get('https://fakestoreapi.com/products');
-        }
-        let endTime = Date.now();
-        let duration = (endTime - startTime) / 1000; // seconds
-        zest.expect(duration).toBeLessThan(2);
-    });
 });
